3b23d5474ef4f1042185133ddeaac6415dbcaa6c
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2013-10-11 14:09:51 -0400

    FIX: referencing sysfiles from package_data

--- anlffr/helper/biosemi2mne.py
+++ anlffr/helper/biosemi2mne.py
@@ -1,8 +1,11 @@
 from subprocess import call
 import mne
 import numpy as np
+import os
+import sys
 
-def importbdf(edfname,fiffname,hptsname,aliasname,evename,refchans):
+def importbdf(edfname, fiffname, evename, refchans,
+              hptsname = None, aliasname = None):
     """ Wrapper around MNE to import Biosemi BDF files
     
     Parameters
@@ -21,9 +24,20 @@ def importbdf(edfname,fiffname,hptsname,aliasname,evename,refchans):
     
     Requires
     --------
-    MNE commandline tools should be in the path of current shell
+    MNE commandline tools should be in the path of current shell.
+    See the MNE manual for information about the alias and hpts files.
     
     """
+    
+    # What happens if they don' specify a hpts/alias file: Use default
+    if(hptsname == None):
+        anlffr_root = os.path.dirname(sys.modules['anlffr'].__file__)
+        hptsname = os.path.join(anlffr_root,'sysfiles/biosemi32.hpts')
+        
+    if(aliasname == None):
+        anlffr_root = os.path.dirname(sys.modules['anlffr'].__file__)
+        aliasname = os.path.join(anlffr_root,'sysfiles/biosemi32alias.txt')
+        
     call(["mne_edf2fiff","--edf",edfname,"--fif",fiffname,"--hpts",hptsname])
     call(["mne_rename_channels","--fif",fiffname,"--alias",aliasname])
     call(["mne_process_raw","--raw",fiffname,"--digtrig","Status",
