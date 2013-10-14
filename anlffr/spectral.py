ac8207ee1979796f76d4ce0e0134c4eb6d3a1da6
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2013-10-13 22:44:14 -0400

    ENH: Adding bootstrap support - no tests yet

--- anlffr/spectral.py
+++ anlffr/spectral.py
@@ -192,3 +192,103 @@ def mtcpca(x,params):
     plv = plv[ind]
     f = f[ind]
     return (plv,f)
+    
+def bootfunc(x,nPerDraw,nDraws, params, func = 'cpca'):
+    """Run spectral functions with bootstrapping over trials
+    
+    Parameters
+    ----------
+    x - Input data (channel x trials x time) or (trials x time)
+    nPerDraw - Number of trials for each draw
+    nDraws - Number of draws
+    params - Dictionary of parameters to use when calling chosen function
+    func - 'cpca' or 'plv' or 'itc' or 'spec', i.e. which to call?
+    
+    Returns
+    -------
+    (plv, vplv, f) for everything except when func == 'spec'
+    (S, N, vS, vN, f) when func == 'spec'
+    A 'v' prefix denotes variance estimate
+    
+    See help for mtcpca(), mtplv() and mtspec() for more details.
+    
+    Notes
+    -----
+    
+    This is not a particularly parallelized piece of code and hence slow.
+    It is provided just so the functionality is there.
+    
+    """
+    
+    if(len(x.shape) == 3):
+        trialdim = 1
+        ntrials = x.shape[trialdim]
+        print 'The data is of format (channels x trials x time)'
+    elif(len(x.shape) == 2):
+        trialdim = 0
+        ntrials = x.shape[trialdim]
+        print 'The data is of format (trials x time) i.e. single channel'
+    else:
+        print 'Sorry! The data should be a 2 or 3 dimensional array!'
+        
+    if(func == 'spec'):
+        S = 0
+        N = 0
+        vS = 0
+        vN = 0
+    else:
+        plv = 0
+        vplv = 0
+
+    for drawnum in np.arange(0,nDraws):
+        inds = np.ceil((np.random.rand(nPerDraw))*ntrials)
+        
+        print 'Doing Draw #',drawnum+1, '/', nDraws
+        
+        if(trialdim == 1):
+            xdraw = x[:,inds,:]
+        elif(trialdim == 0):
+            xdraw = x[inds,:]
+        else:
+            print 'Data not in the right formmat!'
+               
+        if(func == 'spec'):
+            (tempS,tempN,f) = mtspec(xdraw,params)
+            S = S + tempS
+            N = N + tempN
+            vS = vS + tempS**2
+            vN = vN + tempN**2
+        elif(func == 'cpca'):
+            (tempplv,f)  = mtcpca(xdraw,params)
+            plv = plv + tempplv
+            vplv = vplv + tempplv**2
+        elif(func == 'itc'):
+            params['itc'] = 1
+            (plv,f) = mtplv(xdraw,params)
+            plv = plv + tempplv
+            vplv = vplv + tempplv**2
+        elif(func == 'plv'):
+            params['plv'] = 0
+            (plv,f) = mtplv(xdraw,params)
+            plv = plv + tempplv
+            vplv = vplv + tempplv**2
+        else:
+            print 'Unknown func argument!'
+            return
+            
+        
+    if(func == 'spec'):
+        vS = (vS - S**2)/(nDraws - 1)
+        vN = (vN - N**2)/(nDraws - 1)
+        S = S/nDraws
+        N = N/nDraws
+        return (S,N,vS,vN,f)
+    else:
+        vplv = (vplv - plv**2)/(nDraws - 1)
+        plv = plv/nDraws
+        return (plv,vplv,f)
+        
+                
+            
+        
+        
