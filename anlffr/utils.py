4b24c0e4765f2412d3df640355108d5edb922598
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2013-12-05 15:59:34 -0500

    ENH: Initial Addition of Logging Support

---
+++ anlffr/utils.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 14:56:34 2013
+
+@author: Hari Bharadwaj
+"""
+import logging
+import inspect
+from functools import wraps
+
+logger = logging.getLogger('anlffr') # Used across all code
+logger.propagate = False # What to do in case of multiple imports
+
+def verbose(function):
+    """Decorator to allow functions to override default log level
+
+    Do not call this function directly to set the global verbosity level,
+    instead use set_log_level().
+
+    Parameters (to decorated function)
+    ----------------------------------
+    verbose : bool, str, int, or None
+        The level of messages to print. If a str, it can be either DEBUG,
+        INFO, WARNING, ERROR, or CRITICAL. Note that these are for
+        convenience and are equivalent to passing in logging.DEBUG, etc.
+        For bool, True is the same as 'INFO', False is the same as 'WARNING'.
+        None defaults to using the current log level [e.g., set using
+        mne.set_log_level()].
+    """
+    arg_names = inspect.getargspec(function).args
+    # this wrap allows decorated functions to be pickled
+
+    @wraps(function)
+    def dec(*args, **kwargs):
+        # Check if the first arg is "self", if it has verbose, make it default
+        if len(arg_names) > 0 and arg_names[0] == 'self':
+            default_level = getattr(args[0], 'verbose', None)
+        else:
+            default_level = None
+        verbose_level = kwargs.get('verbose', default_level)
+        if verbose_level is not None:
+            old_level = set_log_level(verbose_level, True)
+            # set it back if we get an exception
+            try:
+                ret = function(*args, **kwargs)
+            except:
+                set_log_level(old_level)
+                raise
+            set_log_level(old_level)
+            return ret
+        else:
+            return function(*args, **kwargs)
+
+    # set __wrapped__ attribute so ?? in IPython gets the right source
+    dec.__wrapped__ = function
+
+    return dec
+    
+def set_log_level(verbose=None, return_old_level=False):
+    """Convenience function for setting the logging level
+
+    Parameters
+    ----------
+    verbose : bool, str, int, or None
+        The verbosity of messages to print. If a str, it can be either DEBUG,
+        INFO, WARNING, ERROR, or CRITICAL. Note that these are for
+        convenience and are equivalent to passing in logging.DEBUG, etc.
+        For bool, True is the same as 'INFO', False is the same as 'WARNING'.
+        If None, the environment variable MNE_LOG_LEVEL is read, and if
+        it doesn't exist, defaults to INFO.
+    return_old_level : bool
+        If True, return the old verbosity level.
+    """
+    if verbose is None:
+        verbose = 'WARNING'
+    elif isinstance(verbose, bool):
+        if verbose is True:
+            verbose = 'INFO'
+        else:
+            verbose = 'WARNING'
+    if isinstance(verbose, basestring):
+        verbose = verbose.upper()
+        logging_types = dict(DEBUG=logging.DEBUG, INFO=logging.INFO,
+                             WARNING=logging.WARNING, ERROR=logging.ERROR,
+                             CRITICAL=logging.CRITICAL)
+        if not verbose in logging_types:
+            raise ValueError('verbose must be of a valid type')
+        verbose = logging_types[verbose]
+    logger = logging.getLogger('mne')
+    old_verbose = logger.level
+    logger.setLevel(verbose)
+    return (old_verbose if return_old_level else None)
+
+
