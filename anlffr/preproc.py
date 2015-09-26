9d2b3807b7acf59f2df02fb464f9e7b97970ce3f
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2015-09-25 21:09:42 -0400

    Editing defaults to match mne-python change to API for picks

--- anlffr/preproc.py
+++ anlffr/preproc.py
@@ -14,7 +14,7 @@ from mne.filter import band_pass_filter
 
 @verbose
 def find_blinks(raw, event_id=998, thresh=100e-6, l_freq=0.5, h_freq=10,
-                filter_length='10s', ch_name='A1', tstart=0.,
+                filter_length='10s', ch_name=['A1', ], tstart=0.,
                 l_trans_bandwidth=0.15):
 
     """Utility function to detect blink events from specified channel.
@@ -31,7 +31,7 @@ def find_blinks(raw, event_id=998, thresh=100e-6, l_freq=0.5, h_freq=10,
         High pass frequency.
     filter_length : str | int | None
         Number of taps to use for filtering.
-    ch_name: str | None
+    ch_name: list | None
         If not None, use specified channel(s) for EOG
     tstart : float
         Start detection after tstart seconds.
